c645952569a634784c9d7cd953f4c5356fcbcaa5
Enitoni <enitoni@gmail.com>
2019-08-19 22:33:11 +0200

    add useInstanceEvent

---
+++ src/common/electron/hooks/useInstanceEvent.ts
@@ -0,0 +1,19 @@
+import { useInstance } from "./useInstance"
+import { useEffect } from "react"
+
+export type BrowserWindowEvents = "will-resize" | "maximize" | "unmaximize"
+
+export const useInstanceEvent = (
+  type: BrowserWindowEvents,
+  callback: (event: any) => void
+) => {
+  const instance = useInstance()
+
+  useEffect(() => {
+    instance.on(type as any, callback)
+
+    return () => {
+      instance.removeListener(type as any, callback)
+    }
+  })
+}
