fed105474b563f86b5ddc5a0c511d1dd294d7b7c
Enitoni <enitoni@gmail.com>
2019-08-20 05:56:49 +0200

    use manager provider

--- src/client.tsx
+++ src/client.tsx
@@ -2,10 +2,21 @@ import React from "react"
 import ReactDOM from "react-dom"
 import { App } from "./modules/core/components/App"
 import { remote } from "electron"
+import { createManager } from "./common/state/manager"
+import { managerContext } from "./common/state/contexts/managerContext"
 
-function init() {
+async function init() {
   const element = document.querySelector(".app")
-  ReactDOM.render(<App />, element)
+  const manager = createManager()
+
+  await manager.init()
+
+  ReactDOM.render(
+    <managerContext.Provider value={manager}>
+      <App />
+    </managerContext.Provider>,
+    element
+  )
 
   remote.getCurrentWindow().show()
 }
