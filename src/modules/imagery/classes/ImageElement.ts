df7caead2f2135fb4f1f64c53dbdadb34bfbe0a7
Enitoni <enitoni@gmail.com>
2019-08-20 08:27:25 +0200

    add name getter

--- src/modules/imagery/classes/ImageElement.ts
+++ src/modules/imagery/classes/ImageElement.ts
@@ -1,3 +1,8 @@
 import { SkinElement } from "../../skin/classes/SkinElement"
+import { UPSCALED_TO_DOUBLE_IDENTIFIER } from "../../skin/helpers/filterSkinImages"
 
-export class ImageElement extends SkinElement {}
+export class ImageElement extends SkinElement {
+  public get name() {
+    return super.name.replace(UPSCALED_TO_DOUBLE_IDENTIFIER, "")
+  }
+}
