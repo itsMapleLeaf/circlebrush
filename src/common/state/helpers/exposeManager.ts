31417a0e1f3b01d4cc090b29b36cb74309b1501f
Enitoni <enitoni@gmail.com>
2019-08-20 06:24:56 +0200

    type mistake

--- src/common/state/helpers/exposeManager.ts
+++ src/common/state/helpers/exposeManager.ts
@@ -2,7 +2,7 @@ import { StoreManager } from "../classes/StoreManager"
 
 const safeWindow = window as any
 
-export const exposeManager = (manager: StoreManager, callback: () => {}) => {
+export const exposeManager = (manager: StoreManager, callback: () => void) => {
   safeWindow.__MANAGER__ = manager
   callback()
   safeWindow.__MANAGER__ = undefined
