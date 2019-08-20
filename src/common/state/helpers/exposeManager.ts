92d682300b847b494f2f83e0f0cb2dea0e873632
Enitoni <enitoni@gmail.com>
2019-08-20 06:25:33 +0200

    give this a description

--- src/common/state/helpers/exposeManager.ts
+++ src/common/state/helpers/exposeManager.ts
@@ -2,6 +2,7 @@ import { StoreManager } from "../classes/StoreManager"
 
 const safeWindow = window as any
 
+/** Exposes the manager to the global object during the callback's execution */
 export const exposeManager = (manager: StoreManager, callback: () => void) => {
   safeWindow.__MANAGER__ = manager
   callback()
