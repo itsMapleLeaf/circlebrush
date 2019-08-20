85a7e75a8e708b4fad2badbc4b66b472f91f6c03
Enitoni <enitoni@gmail.com>
2019-08-20 08:27:31 +0200

    name getter

--- src/modules/skin/classes/SkinElement.ts
+++ src/modules/skin/classes/SkinElement.ts
@@ -1,4 +1,13 @@
+import * as path from "path"
+
 /** Represents an element in a skin */
 export abstract class SkinElement {
   constructor(public path: string) {}
+
+  public get name() {
+    const ext = path.extname(this.path)
+    const name = path.basename(this.path, ext)
+
+    return name
+  }
 }
