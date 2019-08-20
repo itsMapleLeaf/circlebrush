2f85a07bce4e23a0907709cc361266c4fc72b14c
Enitoni <enitoni@gmail.com>
2019-08-20 16:46:23 +0200

    small lil mistake

--- src/modules/skin/helpers/humanizeFilename.ts
+++ src/modules/skin/helpers/humanizeFilename.ts
@@ -1,3 +1,3 @@
 export const humanizeFilename = (name: string) => {
-  return (name.charAt(0).toUpperCase() + name.slice(1)).replace(/\-/, " ")
+  return (name.charAt(0).toUpperCase() + name.slice(1)).replace(/\-/g, " ")
 }
