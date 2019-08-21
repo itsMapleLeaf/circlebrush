33c4fc16dd6ab1c19e6f9bbcf14a962837c64456
Enitoni <enitoni@gmail.com>
2019-08-21 21:57:37 +0200

    prettier

--- src/modules/theming/components/ThemeProvider.tsx
+++ src/modules/theming/components/ThemeProvider.tsx
@@ -5,7 +5,5 @@ import { darkTheme, lightTheme } from "../themes"
 export function ThemeProvider(props: { children: any }) {
   const dark = true // Remove comment when this is fixed: remote.systemPreferences.isDarkMode()
 
-  return (
-    <Provider theme={dark ? darkTheme : lightTheme}>{props.children}</Provider>
-  )
+  return <Provider theme={dark ? darkTheme : lightTheme}>{props.children}</Provider>
 }
