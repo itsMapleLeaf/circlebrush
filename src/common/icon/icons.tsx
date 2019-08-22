81d3c7128f2c1d76dabcab56e9111392d912c414
Enitoni <enitoni@gmail.com>
2019-08-22 11:03:13 +0200

    add more icons

--- src/common/icon/icons.tsx
+++ src/common/icon/icons.tsx
@@ -8,11 +8,7 @@ export const close = (
 
 export const maximize = (
   <svg width="16" height="16" viewBox="0 0 16 16">
-    <path
-      fillRule="evenodd"
-      clipRule="evenodd"
-      d="M12 4H4V12H12V4ZM3 3V13H13V3H3Z"
-    />
+    <path fillRule="evenodd" clipRule="evenodd" d="M12 4H4V12H12V4ZM3 3V13H13V3H3Z" />
   </svg>
 )
 
@@ -29,5 +25,31 @@ export const restore = (
       clipRule="evenodd"
       d="M5 2H14V3V11H13V3H5V2ZM4 5H11V12H4V5ZM3 4H4H11H12V5V12V13H11H4H3V12V5V4Z"
     />
+  </svg>
+)
+
+export const elements = (
+  <svg width="24" height="24" viewBox="0 0 24 24">
+    <path d="M0 0H6V6H0V0Z" />
+    <path d="M9 0H15V6H9V0Z" />
+    <path d="M0 9H6V15H0V9Z" />
+    <path d="M0 18H6V24H0V18Z" />
+    <path d="M9 9H15V15H9V9Z" />
+    <path d="M18 18H24V24H18V18Z" />
+    <path d="M18 9H24V15H18V9Z" />
+    <path d="M9 18H15V24H9V18Z" />
+    <path d="M18 0H24V6H18V0Z" />
+  </svg>
+)
+
+export const wrench = (
+  <svg width="24" height="24" viewBox="0 0 24 24">
+    <path d="M22.7 19l-9.1-9.1c.9-2.3.4-5-1.5-6.9-2-2-5-2.4-7.4-1.3L9 6 6 9 1.6 4.7C.4 7.1.9 10.1 2.9 12.1c1.9 1.9 4.6 2.4 6.9 1.5l9.1 9.1c.4.4 1 .4 1.4 0l2.3-2.3c.5-.4.5-1.1.1-1.4z" />
+  </svg>
+)
+
+export const eye = (
+  <svg width="24" height="24" viewBox="0 0 24 24">
+    <path d="M12 4.5C7 4.5 2.73 7.61 1 12c1.73 4.39 6 7.5 11 7.5s9.27-3.11 11-7.5c-1.73-4.39-6-7.5-11-7.5zM12 17c-2.76 0-5-2.24-5-5s2.24-5 5-5 5 2.24 5 5-2.24 5-5 5zm0-8c-1.66 0-3 1.34-3 3s1.34 3 3 3 3-1.34 3-3-1.34-3-3-3z" />
   </svg>
 )
