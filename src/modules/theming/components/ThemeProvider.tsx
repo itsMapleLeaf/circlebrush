936d4d17a65bb28e8ef65f05c21ef51586a075e5
kingdaro <kingdaro@gmail.com>
2019-08-22 23:26:13 -0500

    children: React.ReactNode

--- src/modules/theming/components/ThemeProvider.tsx
+++ src/modules/theming/components/ThemeProvider.tsx
@@ -1,8 +1,8 @@
-import React from "react"
 import { ThemeProvider as Provider } from "emotion-theming"
+import React from "react"
 import { darkTheme, lightTheme } from "../themes"
 
-export function ThemeProvider(props: { children: any }) {
+export function ThemeProvider(props: { children: React.ReactNode }) {
   const dark = true // Remove comment when this is fixed: remote.systemPreferences.isDarkMode()
 
   return <Provider theme={dark ? darkTheme : lightTheme}>{props.children}</Provider>
