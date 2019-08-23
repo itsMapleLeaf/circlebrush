f6701ae61c4521607dc59aec08f8d12a69bf557a
Enitoni <enitoni@gmail.com>
2019-08-23 03:55:51 +0200

    Fix animation parsing

--- src/modules/imagery/helpers/parseImagePaths.ts
+++ src/modules/imagery/helpers/parseImagePaths.ts
@@ -7,22 +7,39 @@ import { extname, basename } from "path"
 import { ImageElementData } from "../types/ImageElementData"
 import { getStrippedFilename } from "../../../common/lang/string/getStrippedFilename"
 
+export interface FrameData {
+  canonical: string
+  index: number
+}
+
 export interface ImageParseData {
   name: string
   path: string
-  frameFor?: string
+  frame?: FrameData
 }
 
 /**
- * Returns a string of the canonical if the image is a valid frame
+ * Returns a frame data if the image is a valid frame
  */
-export const getFrameFor = (name: string): string | undefined => {
-  const isEdgecase = ANIMATION_NAMING_EDGECASES.includes(name)
-  const regex = new RegExp(isEdgecase ? `\\d+` : `-\\d+`)
+export const getFrameFor = (name: string): FrameData | undefined => {
+  /**
+   * Check if this frame is one of the edgecases
+   * where the frame number is not prefixed with a hyphen,
+   * we remove the number from the frame so that
+   * it'll match the edgecase list
+   */
+  const isEdgecase = ANIMATION_NAMING_EDGECASES.includes(name.replace(/\d+/, ""))
 
-  if (name.match(regex)) {
+  const regex = new RegExp(isEdgecase ? /\d+/ : /-\d+/)
+  const match = name.match(regex)
+
+  if (match) {
     const canonical = name.replace(regex, "")
-    return canonical
+
+    return {
+      canonical,
+      index: Number(match[0].replace("-", "")),
+    }
   }
 }
 
@@ -31,9 +48,9 @@ export const getFrameFor = (name: string): string | undefined => {
  */
 export const parseImagePath = (path: string) => {
   const name = getStrippedFilename(path).replace(HD_SUFFIX, "")
-  const frameFor = getFrameFor(name)
+  const frame = getFrameFor(name)
 
-  return { name, path, frameFor }
+  return { name, path, frame }
 }
 
 /**
@@ -43,11 +60,27 @@ export const createDataFromParse = (
   data: ImageParseData,
   others: ImageParseData[]
 ): ImageElementData | undefined => {
-  const { name, path, frameFor } = data
-  const frames = others.filter(other => other.frameFor === data.name).length
+  const { name, path, frame } = data
 
-  /** Return undefined for elements we don't want to include */
-  if (frameFor) return
+  /** Get the amount of frames associated with this element */
+  const frames = others.filter(
+    other => other.frame && other.frame.canonical === data.name
+  ).length
+
+  /** Include this if it's the first frame and there is not a static variant */
+  if (frame) {
+    const { canonical, index } = frame
+    const hasStatic = others.find(other => other.name === canonical)
+
+    /** Skip this if there's a static, which will be the parent, or if there's not but this is the first frame, which will be the parent. */
+    if (hasStatic || index !== 0) return
+
+    return {
+      name: canonical,
+      frames,
+      path,
+    }
+  }
 
   return {
     name,
