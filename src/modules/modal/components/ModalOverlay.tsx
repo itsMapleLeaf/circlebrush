882c625ab810359efb49e7d702706139a1a250a3
Enitoni <enitoni@gmail.com>
2019-08-24 05:46:27 +0200

    Hide overlay unless modals are open

--- src/modules/modal/components/ModalOverlay.tsx
+++ src/modules/modal/components/ModalOverlay.tsx
@@ -5,16 +5,29 @@ import { styled } from "../../theming/themes"
 import { getTransparency } from "../../theming/helpers"
 import { ModalItem } from "./ModalItem"
 
-const Container = styled.div`
+const Container = styled.div<{ active?: boolean }>`
   ${cover()}
   background: ${getTransparency("negative")};
+
+  pointer-events: none;
+  opacity: 0;
+
+  ${props =>
+    props.active &&
+    `
+    opacity: 1;
+    pointer-events: all;
+  `}
 `
 
 export function ModalOverlay() {
-  const modals = useObservedStore(({ modalStore }) => modalStore.modals)
+  const { modals, active } = useObservedStore(({ modalStore }) => ({
+    modals: modalStore.modals,
+    active: modalStore.modals.length > 0,
+  }))
 
   return (
-    <Container>
+    <Container active={active}>
       {modals.map(modal => (
         <ModalItem modal={modal} />
       ))}
